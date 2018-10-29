83a1db7e06c080304301ebcf5ccbfd631f64c8cd
Deven Bansod <devenbansod.bits@gmail.com>
2018-10-29 10:58:21 +0530

    Fix issue with setup. Persist setup
    Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-alias git-pr-local='sh $(pwd)/git-pr-local.sh'
+CURRENT_PATH=$(pwd)
+alias git-pr-local='sh $CURRENT_PATH/git-pr-local.sh'
+
+echo "\nalias git-pr-local='sh $CURRENT_PATH/git-pr-local.sh'" >> ~/.bashrc
+echo "\nalias git-pr-local='sh $CURRENT_PATH/git-pr-local.sh'" >> ~/.bash_profile
+echo "\nalias git-pr-local='sh $CURRENT_PATH/git-pr-local.sh'" >> ~/.profile
