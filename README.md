810d000552f6c10631c8f64d5c601d542bac3ead
Deven Bansod <devenbansod.bits@gmail.com>
2018-10-29 10:17:50 +0530

    Add optional params for remote and local branch name

--- README.md
+++ README.md
@@ -3,5 +3,18 @@ Script to checkout any PR for local review
 
 ## Usage
 ```sh
-./git-pr.sh <base-branch> <remote> <pr-number> <new-local-branch>
+./git-pr.sh <pr-number> [[<remote>] <new-local-branch>]
+```
+
+### Default values
+* remote: `origin`
+* new-local-branch: `pr/<pr-number>`
+
+
+### Example:
+* Let's assume `origin` points to the central repository, which has the issues and PRs tracker
+* You want to locally checkout a PR with ID 2018 to a new local branch named `pr-local-checkout-2018` 
+
+```sh
+./git-pr.sh 2018 origin pr-local-checkout-2018
 ```
--- git-pr.sh
+++ git-pr.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
-# Usage: ./git-pr.sh <base-branch> <remote> <pr-number> <new-local-branch>
+# Usage: ./git-pr.sh <pr-number> [[<remote>] <new-local-branch>]
 
-git checkout $1 && git pull \
-&& git fetch $2 refs/pull/$3/head:$4 \
-&& git checkout $4
+REMOTE="origin"
+LOCAL_BRANCH_PREFIX="pr/"
+
+show_usage()
+{
+	echo "\nUsage: ./git-pr.sh <pr-number> [[<remote>] <new-local-branch>]"
+}
+
+if [ -z "$1" ]; then
+	echo "No PR Number specified"
+	show_usage
+	exit
+else
+	LOCAL_BRANCH=$LOCAL_BRANCH_PREFIX$1
+fi
+
+if [ -z "$2" ]; then
+	echo "No remote specified. Using default: $REMOTE"
+else
+	REMOTE=$2
+fi
+
+if [ -z "$3" ]; then
+	echo "No local branch name specified. Using default: $LOCAL_BRANCH"
+else
+	LOCAL_BRANCH=$3
+fi
+
+git fetch $REMOTE refs/pull/$1/head:$LOCAL_BRANCH \
+&& git checkout $LOCAL_BRANCH
